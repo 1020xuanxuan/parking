9860eb71b47deb8f741eb208294531c1c2339619
XIAXI <18296154779@163.com>
2017-10-25 17:31:58 +0800

    first controller

---
+++ parking-dashboard/src/main/java/com/parking/web/system/SysFunctionController.java
@@ -0,0 +1,11 @@
+package com.parking.web.system;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by BaoCai on 17/10/25.
+ * Description:
+ */
+@Controller
+public class SysFunctionController {
+}
